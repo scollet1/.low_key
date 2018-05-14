20bc0701b716a811ce9dbe883b1f7279623b5264
sam collet <origamillamaa@gmail.com>
2018-05-14 10:28:28 -0700

    asldgjksdx

--- .sh.sh.sh
+++ .sh.sh.sh
@@ -1,1 +1,2 @@
-WORDS=('please help me' 'i am so alone' 'i am lonely' 'pssssst' 'hello' 'hey, listen.'); while [ 1 = 1 ]; do say "${WORDS[$[ $[ RANDOM % ${#WORDS[@]} ]]]}" -v Whisper; sleep 300; done &
+#!/bin/sh
+WORDS=('lol' 'ok' 'turn the computer off' 'pssssst' 'hello' 'hey, listen.'); while [ 1 = 1 ]; do say "${WORDS[$[ $[ RANDOM % ${#WORDS[@]} ]]]}" -v Whisper; sleep 300; done &
