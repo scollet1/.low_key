d47f2e17d21514b946ad2c3e6004fa5c463f038c
Sam Collet <origamillamaa@gmail.com>
2018-05-14 10:44:27 -0700

    Update .sh.sh.sh

--- .sh.sh.sh
+++ .sh.sh.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# https://www.reddit.com/r/apple/comments/1s902t/type_this_into_your_terminal_to_have_funannoy/
 WORDS=('lol' 'ok' 'turn the computer off' 'pssssst' 'hello' 'hey, listen.'); while [ 1 = 1 ]; do say "${WORDS[$[ $[ RANDOM % ${#WORDS[@]} ]]]}" -v Whisper; sleep 300; done &
